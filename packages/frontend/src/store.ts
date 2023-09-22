77539573a9e8135b14d8fd6b80cbc8f337a49b3d
Mar0xy <marie@kaifa.ch>
2023-09-22 04:06:22 +0200

    upd: change default themes

--- packages/frontend/src/store.ts
+++ packages/frontend/src/store.ts
@@ -388,8 +388,8 @@ interface Watcher {
 /**
  * 常にメモリにロードしておく必要がないような設定情報を保管するストレージ(非リアクティブ)
  */
-import lightTheme from '@/themes/l-light.json5';
-import darkTheme from '@/themes/d-green-lime.json5';
+import lightTheme from '@/themes/l-cherry.json5';
+import darkTheme from '@/themes/d-transfem-cherry.json5';
 
 export class ColdDeviceStorage {
 	public static default = {
