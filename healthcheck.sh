ea3cb92f77fe8b103cf1fe47e45502ba9a06145f
Amelia Yukii <123300075+Insert5StarName@users.noreply.github.com>
2023-09-23 04:45:07 +0200

    fix: healtcheck.sh

--- healthcheck.sh
+++ healthcheck.sh
@@ -3,5 +3,5 @@
 # SPDX-FileCopyrightText: syuilo and other misskey contributors
 # SPDX-License-Identifier: AGPL-3.0-only
 
-PORT=$(grep '^port:' /misskey/.config/default.yml | awk 'NR==1{print $2; exit}')
+PORT=$(grep '^port:' /sharkey/.config/default.yml | awk 'NR==1{print $2; exit}')
 curl -s -S -o /dev/null "http://localhost:${PORT}"
