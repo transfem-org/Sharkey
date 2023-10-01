54591819011d138796ab7e3e7103237be627cb5b
Mar0xy <marie@kaifa.ch>
2023-10-01 04:04:13 +0200

    fix: typo

--- packages/frontend/src/components/MkNote.vue
+++ packages/frontend/src/components/MkNote.vue
@@ -373,7 +373,7 @@ function like(): void {
 	pleaseLogin();
 	showMovedDialog();
 	os.api('notes/reactions/create', {
-		noteId: apperNote.id,
+		noteId: appearNote.id,
 		reaction: '❤️',
 	});
 	const el = likeButton.value as HTMLElement | null | undefined;
