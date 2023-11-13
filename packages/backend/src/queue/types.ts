e4bd74183fc4bf4b3e7d60549875803da3a43537
Mar0xy <marie@kaifa.ch>
2023-11-13 13:09:21 +0100

    chore: remove type in DbNoteImportToDbJobData

--- packages/backend/src/queue/types.ts
+++ packages/backend/src/queue/types.ts
@@ -110,7 +110,6 @@ export type DbUserImportToDbJobData = {
 export type DbNoteImportToDbJobData = {
 	user: ThinUser;
 	target: any;
-	note?: MiNote['id'] | null;
 };
 
 export type DbKeyNoteImportToDbJobData = {
