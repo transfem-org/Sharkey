13600e0abc794256fcce41bc439cde7f4d8917d7
Mar0xy <marie@kaifa.ch>
2023-11-17 15:17:47 +0100

    upd: keep choice in `indexable` migration update

--- packages/backend/migration/1700228972000-update-indexable.js
+++ packages/backend/migration/1700228972000-update-indexable.js
@@ -4,11 +4,11 @@ export class UpdateIndexable1700228972000 {
     async up(queryRunner) {
         await queryRunner.query(`ALTER TABLE "user" RENAME COLUMN "isIndexable" TO "noindex"`);
         await queryRunner.query(`ALTER TABLE "user" ALTER COLUMN "noindex" SET DEFAULT false`);
-        await queryRunner.query(`UPDATE "user" SET "noindex" = false WHERE "noindex" = true`);
+        await queryRunner.query(`UPDATE "user" SET "noindex" = NOT "noindex"`);
     }
 
     async down(queryRunner) {
-        await queryRunner.query(`UPDATE "user" SET "noindex" = true WHERE "noindex" = false`);
+        await queryRunner.query(`UPDATE "user" SET "noindex" = NOT "noindex"`);
         await queryRunner.query(`ALTER TABLE "user" ALTER COLUMN "noindex" SET DEFAULT true`);
         await queryRunner.query(`ALTER TABLE "user" RENAME COLUMN "noindex" TO "isIndexable"`);
     }
