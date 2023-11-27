92249a15f9f023fcd68257e943cf10be556a9442
moshibar <25570708+MoshiBar@users.noreply.github.com>
2023-11-27 13:07:28 -0600

    KaTeX support

---
+++ packages/frontend/src/components/MkFormula.vue
@@ -0,0 +1,25 @@
+<template>
+	<XFormula :formula="formula" :block="block" />
+</template>
+
+<script lang="ts">
+import { defineAsyncComponent, defineComponent } from "vue";
+
+export default defineComponent({
+	components: {
+		XFormula: defineAsyncComponent(
+			() => import("@/components/MkFormulaCore.vue"),
+		),
+	},
+	props: {
+		formula: {
+			type: String,
+			required: true,
+		},
+		block: {
+			type: Boolean,
+			required: true,
+		},
+	},
+});
+</script>
